fc16590a419995fb3522500f174fe78c12ed8c50
Sanderpeeters2005 <511800@vistacollege.nl>
2025-06-05 11:26:20 +0200

    22

--- includes/navbar.php
+++ includes/navbar.php
@@ -7,13 +7,13 @@
         </div>
         <nav id="nav-menu">
             <ul>
-                <li><a href="index.php">Home</a></li>
-                <li><a href="galerij.php">Galerij</a></li>
-                <li><a href="evenementen.php">Evenementen</a></li>
-                <li><a href="koningspaar.php">Koningspaar</a></li>
-                <li><a href="nieuws.php">Nieuws</a></li>
-                <li><a href="contact.php">Contact</a></li>
-                <li><a href="historie.php">Historie</a></li>
+                <li><a href="../index.php">Home</a></li>
+                <li><a href="pages/galerij.php">Galerij</a></li>
+                <li><a href="pages/evenementen.php">Evenementen</a></li>
+                <li><a href="pages/koningspaar.php">Koningspaar</a></li>
+                <li><a href="pages/nieuws.php">Nieuws</a></li>
+                <li><a href="pages/contact.php">Contact</a></li>
+                <li><a href="pages/historie.php">Historie</a></li>
             </ul>
         </nav>
         <div class="menu-toggle" onclick="toggleMenu()">☰</div>
