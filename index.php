1d7102b978f44d4c918430d963a7a609470d8005
Gino T <7040volt@gmail.com>
2025-06-05 13:58:56 +0200

    Update

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Schutterij St. George</title>
+    <title>Home | ST. George</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="assets/css/style.css">
 </head>
--- pages/galerij.php
+++ pages/galerij.php
@@ -6,7 +6,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Fotogalerij</title>
+    <title>Fotogalerij | ST. George</title>
     <script src="https://cdn.tailwindcss.com"></script>
     <link rel="stylesheet" href="<?= $base_url ?>assets/css/style.css">
 </head>
